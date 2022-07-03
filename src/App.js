5c2b18f549621a2c9a2bbce49ee58501a1887593
ramisha-anwer <ramishaaa27@gmail.com>
2022-07-03 23:54:28 +0500

    login and half home done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './components/Home';
+import Header from './components/Header';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Routes>
+          <Route  path='/' element={<Login />}></Route>
+          <Route exact path="/home" element={<><Header/><Home/></>}/>
+          
+        </Routes>
     </div>
+    </Router>
   );
 }
 
